0d44b0209fa4c07d8227195d2f691f65ece02a24
Saqib Javed <jvedsaqib6291@gmail.com>
2023-03-15 00:08:26 +0530

    Added splashscreen

--- app/src/main/java/com/example/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.restaurantmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getSupportActionBar().hide();
+
+        // for fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // below code is to start splashing
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,
+                        Home.class));
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
